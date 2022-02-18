d50e6abe65dfb6a3937da6b912095cc637155b5f
Rita Cunha <rita.cunha.ts@gmail.com>
2022-02-18 12:46:32 +0000

    hidden links

--- routes/index.js
+++ routes/index.js
@@ -1,8 +1,11 @@
 const router = require("express").Router();
 
+const isLoggedIn =require("./../middleware/isLoggedIn")
+
 /* GET home page */
 router.get("/", (req, res, next) => {
-  res.render("index");
+  const user = req.session.user;
+  res.render("index", {user});
 });
 
 module.exports = router;
--- routes/profile.js
+++ routes/profile.js
@@ -4,15 +4,19 @@ const mongoose = require("mongoose");
 
 const Game = require("../models/Game.model");
 
+const isLoggedIn = require("./../middleware/isLoggedIn");
+
 // Require the User model in order to interact with the database
 const User = require("../models/User.model");
 
 router.get("/profile", (req, res, next) => {
-  res.render("profile");
+  const user = req.session.user;
+  res.render("profile", {user});
 });
 
 router.get("/create-profile", (req, res, next) => {
-  res.render("create-profile");
+  const user = req.session.user;
+  res.render("create-profile", {user});
 });
 
 module.exports = router;
