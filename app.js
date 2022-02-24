223671ee7d7ab874ad773f3c86a25831306cf78b
Rita Cunha <rita.cunha.ts@gmail.com>
2022-02-24 22:39:35 +0000

    css for profile-edit

--- app.js
+++ app.js
@@ -57,60 +57,69 @@ app.use(function (req, res, next) {
 });
 
 (async () => {
-    if( await Game.countDocuments({})){
-        return
-    }
+  // if (await Game.countDocuments({})) {
+  //   return;
+  // }
 
-    let response = await axios.get(
-      "https://steamspy.com/api.php?request=top100in2weeks"
-    );
-    //console.log(Object.values(response.data)[0])
+  // let response = await axios.get(
+  //   "https://steamspy.com/api.php?request=top100in2weeks"
+  // );
+  //console.log(Object.values(response.data)[0])
 
-    let games = Object.values(response.data); 
+  // let games = Object.values(response.data);
+  let games = [
+    1307710, 359550, 1142710, 1551360, 362003, 1190000, 690790, 266410, 1190170,
+    1569040, 431240, 1506830, 686810, 1659040, 440, 1144200, 268910, 367520,
+    881100, 1426210, 1057090, 261570, 588650, 1599340, 534380, 1446780, 1203220,
+    1085660, 252490, 1091500, 1222670, 275850,
+  ];
 
-    for (let i = 0; i < 100; i++) {
+  for (let i = 0; i < games.length; i++) {
     let gameDetails = await axios.get(
       "https://store.steampowered.com/api/appdetails",
       {
         params: {
-          appids: games[i].appid,
-          key:process.env.STEAMKEY,
+          appids: games[i],
+          key: process.env.STEAMKEY,
           language: "eng",
-        }
+        },
       }
-    );    
+    );
     await sleep(500);
-    if (gameDetails.data[games[i].appid].success && (gameDetails.data[games[i].appid].data.type === "game") ) {
-        let data = gameDetails.data[games[i].appid].data;
-        let steam_appid = data.steam_appid;
-        //console.log(gameDetails[games[i].appid]);        
-        let name = data.name;
-        let genres = data.genres;
-        let about_the_game = data.about_the_game;
-        let header_image = data.header_image;
-        let release_date = data.release_date;
-        //console.log(data.metacritic)
-        let metacritic_score = data.metacritic ? data.metacritic.score : 0;
-        let website = data.website;
-        Game.create({
-          steam_appid,
-          name,
-          genres,
-          about_the_game,
-          header_image,
-          release_date,
-          metacritic_score,
-          website,
+    if (
+      gameDetails.data[games[i]].success &&
+      gameDetails.data[games[i]].data.type === "game"
+    ) {
+      let data = gameDetails.data[games[i]].data;
+      let steam_appid = data.steam_appid;
+      //console.log(gameDetails[games[i]]);
+      let name = data.name;
+      let genres = data.genres;
+      let about_the_game = data.about_the_game;
+      let header_image = data.header_image;
+      let release_date = data.release_date;
+      //console.log(data.metacritic)
+      let metacritic_score = data.metacritic ? data.metacritic.score : 0;
+      let website = data.website;
+      Game.create({
+        steam_appid,
+        name,
+        genres,
+        about_the_game,
+        header_image,
+        release_date,
+        metacritic_score,
+        website,
+      })
+        .then((game) => {
+          console.log("created:", game.name);
+          //res.redirect("");
         })
-          .then((game) => {
-            console.log("created:", game.name);
-            //res.redirect("");
-          })
-          .catch((err) => {
-            console.log(err);
-          });
+        .catch((err) => {
+          console.log(err);
+        });
     }
-    }
+  }
 })();
 
 function sleep(ms) {
